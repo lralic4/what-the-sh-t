8249b3a7906a8a6959a1c0fdf5abb808d0b31243
Luka <lralic4@gmail.com>
2024-11-06 10:33:27 -0500

    created basic ability to send image and return score

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import numpy as np
 import os
 
 app = Flask(__name__)
-img_height = 180
-img_width = 180
-class_names = ['type-1', 'type-2', 'type-3', 'type-4', 'type-5', 'type-6', 'type-7']
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+CLASS_NAMES = ['type-1', 'type-2', 'type-3', 'type-4', 'type-5', 'type-6', 'type-7']
 ALLOWED_EXTENSIONS = {'jpeg'}
+MODEL = tf.keras.models.load_model('./bristol-model/bristol-model.h5')
 
 @app.route('/')
 def home():
@@ -17,8 +18,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/img', methods = ['POST'])
-def upload_image():
+def save_image(request):
 
     if 'file' not in request.files:
         flash('No file part')
@@ -30,32 +30,35 @@ def upload_image():
         flash('No selected file')
         return redirect(request.url)
     if file and allowed_file(file.filename):
-        print(type(file))
-        file.save('./temp.jpeg')
-        return {'message': 'success'}    
+        file.save('./temp.jpeg')  
 
 @app.route('/bristol-chart', methods=['POST'])
 def get_bristol_chart_classification():
-    model = tf.keras.models.load_model('./bristol-model/bristol-model.h5')
+    save_image(request)
 
-    fullPath = os.path.abspath("./bristol-model/testimage1.jpeg")  # or similar, depending on your scenario
-    image_path = tf.keras.utils.get_file('testimage1.jpeg', 'file://'+fullPath)
+    fullPath = os.path.abspath("./temp.jpeg")  # or similar, depending on your scenario
+    image_path = tf.keras.utils.get_file('temp.jpeg', 'file://'+fullPath)
 
     img = tf.keras.utils.load_img(
-        image_path, target_size=(img_height, img_width)
+        image_path, target_size=(IMG_HEIGHT, IMG_WIDTH)
     )
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-    predictions = model.predict(img_array)
+    predictions = MODEL.predict(img_array)
     score = tf.nn.softmax(predictions[0])
 
     print(
         "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
+        .format(CLASS_NAMES[np.argmax(score)], 100 * np.max(score))
     )
 
-    return jsonify({'classification': class_names[np.argmax(score)], 'confidence': 100 * np.max(score)})
+    context = {
+        'class': CLASS_NAMES[np.argmax(score)],
+        'confidence': 100 * np.max(score)
+    }
+
+    return render_template('results.html', **context)
 
 if __name__ == '__main__':
     app.run()
